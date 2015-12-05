2f0e5521d5e1e5d76a6fad3632cb7b6b5f0dea34
Parth Patel <parthpatel.org@gmail.com>
2015-12-05 18:52:07 -0500

    Created Buffer Mananger for DB files

---
+++ src/DBBufferManager.c
@@ -0,0 +1,40 @@
+/*
+ * DBBufferManager.c
+ *
+ *  Created on: Dec 5, 2015
+ *      Author: parth
+ */
+
+#include "DBBufferManager.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_record_buffer(int no_of_elements, Record **new_buffer) {
+	*new_buffer = (Record *) malloc(no_of_elements * sizeof(Record)) ;
+
+	if (*new_buffer == NULL) {
+		fprintf(stderr, "Buffer Creation error at process: %d\nSystem will now terminate\n", my_rank);
+		//TODO
+		//terminate_system();
+	}
+
+}
+void expand_buffer(int no_of_elements, Record **buffer_begin, Record **buffer_current) {
+	int new_no_of_elements = no_of_elements * 2;
+	int i;
+	Record *old_buffer_iterator = *buffer_begin;
+	Record *new_buffer_iterator;
+	Record *temp = *buffer_begin;
+
+	get_record_buffer(new_no_of_elements, buffer_begin);
+
+	new_buffer_iterator = *buffer_begin;
+	for (i = 0; i != no_of_elements; ++i) {
+		*new_buffer_iterator = *old_buffer_iterator;
+		++old_buffer_iterator;
+		++new_buffer_iterator;
+	}
+	*buffer_current = new_buffer_iterator;
+	free(temp);
+}
+
---
+++ src/DBBufferManager.h
@@ -0,0 +1,17 @@
+/*
+ * DBBufferManager.h
+ *
+ *  Created on: Dec 5, 2015
+ *      Author: parth
+ */
+
+#ifndef DBBUFFERMANAGER_H_
+#define DBBUFFERMANAGER_H_
+
+#include "Record.h"
+
+extern int my_rank;
+
+void get_record_buffer(int size, Record **new_buffer);
+void expand_buffer(int no_of_elements, Record **buffer_begin, Record **buffer_current);
+#endif /* DBBUFFERMANAGER_H_ */
